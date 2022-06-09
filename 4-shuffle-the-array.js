ba9e4b74e1dc79a52f1a9cbfce1202b35d2e397b
Haris Alkarni <49621897+harisalkarni@users.noreply.github.com>
2022-06-09 10:39:41 +0700

    Update 4-shuffle-the-array.js

--- 4-shuffle-the-array.js
+++ 4-shuffle-the-array.js
@@ -4,5 +4,10 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
+    let a = []; // create empty string
     
+    for (let i = 0 ; i < n; i++) {. //loop through array
+        a.push(nums[i], nums[i+n]); // if the number is find push to new array
+    }
+    return a;
 };
