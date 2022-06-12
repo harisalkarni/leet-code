69e4e2cce5ca8458ba35525b060e6130f6dc9f1d
Haris Alkarni <49621897+harisalkarni@users.noreply.github.com>
2022-06-12 23:22:51 +0700

    Update 5-merge-two-sorted-lists.js

--- 5-merge-two-sorted-lists.js
+++ 5-merge-two-sorted-lists.js
@@ -45,3 +45,28 @@ var mergeTwoLists = function(l1, l2) {
     
     return mergedHead.next;
 };
+
+//EASY TO UNDERSTAND
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if(!l1 || !l2) return (l1? l1:l2);
+  if(l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
