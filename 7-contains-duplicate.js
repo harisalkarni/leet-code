6ede8173ccdb954e1a4345c287d97a98555ee739
Haris Alkarni <49621897+harisalkarni@users.noreply.github.com>
2022-06-13 16:21:47 +0700

    Update 7-contains-duplicate.js

--- 7-contains-duplicate.js
+++ 7-contains-duplicate.js
@@ -9,4 +9,30 @@ var containsDuplicate = function(nums) {
 // another 1 line solution
  var containsDuplicate = function(nums) {
     return nums.sort().some((a, i) => a === nums[i - 1]);
-};
+};   
+
+
+//faster solution
+
+var containsDuplicate = function(nums) {
+    var obj = {}
+    
+    for(i = 0; i < nums.length; i++){
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        if(obj[nums[i]] > 1) return true;
+    }
+    return false;
+}
+
+//more faster and easy to understand
+var containsDuplicate = function(nums){
+    const set = new Set();
+    
+    for(i = 0; i < nums.length; i++){
+        if(set.has(nums[i])){
+            return true;
+        }
+        set.add(nums[i]);
+    }
+    return false;
+}
