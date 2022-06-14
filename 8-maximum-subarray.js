b14c2ba58907ec0aec634185244898fd1af09242
Haris Alkarni <49621897+harisalkarni@users.noreply.github.com>
2022-06-14 10:52:28 +0700

    Update 8-maximum-subarray.js

--- 8-maximum-subarray.js
+++ 8-maximum-subarray.js
@@ -4,4 +4,8 @@
  */
 var maxSubArray = function(nums) {
     
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+  }
+  return Math.max(...nums)
 };
