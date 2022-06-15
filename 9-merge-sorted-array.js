1825a8cfae3ad677a7a87acbffa8cf1debfaa0ac
Haris Alkarni <49621897+harisalkarni@users.noreply.github.com>
2022-06-15 19:58:09 +0700

    Update 9-merge-sorted-array.js

--- 9-merge-sorted-array.js
+++ 9-merge-sorted-array.js
@@ -6,5 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
+    var insertPos = m + n - 1;
+    m--; n--;
+    while (n >= 0) {
+        nums1[insertPos--] = (nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--];
+    }
 };
