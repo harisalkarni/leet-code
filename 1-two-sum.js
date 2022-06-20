57577f2073c4fd4e1b18f35f615c2eef18e71c53
Haris Alkarni <49621897+harisalkarni@users.noreply.github.com>
2022-06-20 18:42:43 +0700

    Update 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -11,3 +11,18 @@ var twoSum = function(nums, target) {
        };
     };
 };
+
+
+//FASTER SOLUTION
+
+var twoSum = function(nums, target) {
+    if (nums.length === 2) return [0, 1];
+    const len = nums.length;
+    let map = {};
+    for(let i = 0; i < len; i++) {
+        let n = target - nums[i];
+        let find = map[n];
+        if(find !== undefined) return [find, i];
+        else map[nums[i]] = i;
+    }
+};
