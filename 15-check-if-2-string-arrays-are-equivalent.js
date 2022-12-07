bc202409fdb97949d2889d52a80a87b48ac90a53
harisalkarni <haris.alkarni@gmail.com>
2022-12-07 13:49:48 +0700

    15. check if two string arrays are equivalent

---
+++ 15-check-if-2-string-arrays-are-equivalent.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+ var arrayStringsAreEqual = function(word1, word2) {
+
+    // one line code
+    return word1.join("") === word2.join("");
+    
+};
+
+// iteration method
+var arrayStringsAreEqual = function(word1, word2) {
+
+    let str1 = "";
+    let str2 = "";
+
+    for(let i = 0; i < word1.length; i++){
+        str1 = str1 + word1[i];
+    }
+    for(let i = 0; i < word2.length; i++){
+        str2 = str2 + word2[i];
+    }
+    return str1 === str2;
+    
+};
